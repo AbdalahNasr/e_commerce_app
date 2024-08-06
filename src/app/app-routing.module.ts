1a941f6ec352d4db2e4a5dd4d983c3b154e4a3a4
abdallah <body16nasr16bn@gmail.com>
2024-08-06 19:45:12 +0300

    add filteration section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '' ,
+    component:SideBarComponent
+},
+  {
+    path: 'product' ,
+    component:ProductPageComponent,
+    children:[
+     { path:'product/productList',
+      component:ProductListComponent
+    },
+    ]
+},  {
+  path: '**' ,
+  component:PageNotFoundComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  
+  
+}
+// {
+//     path: '',
+//     component:LayoutComponent,
+//     children: [
+// {
+//         path:'dashboard',
+//         component:DashboardComponent,   
+//         canActivate:[authGuard] 
+//    }
+//     ]
+// },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    ProductListComponent,
+    ProductPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
